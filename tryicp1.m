1a0d670f86d323ff96d02872cb586c547ac97963
Joshua Manela <jmmanela@mtu.edu>
2015-04-21 16:30:23 -0400

    fixed some stuff for anuj

--- tryicp1.m
+++ tryicp1.m
@@ -9,11 +9,13 @@ profile clear
 profile on
 
 % Load dataset
-% load hallroomvn.mat
+%load anujdata.mat
 
 error = [0;0;0];
 pose=[0;0;0];
 pose_dot=[];
+
+timestamps = [];
 
 % This is what I am the last scan to, whether it be the full map or just
 % scan-by-scan
@@ -26,7 +28,7 @@ temp_scan = [];
 %intitialize the graph structure
 graph.n = 150;
 graph.m = 150;
-graph.width = 1; %meters
+graph.width = 0.75; %meters
 graph.nodemat = repmat(struct('data', [], 'visited', 0, 'n', 0), graph.n, graph.m);
 
 %initialize the first node
@@ -40,23 +42,23 @@ Hokuyo_freq = 40; %Hz
 LidarRange = 30;
 nScanIndex = unique(Lidar_ScanIndex);
 %radius to filter out points when running icp
-filter_radius = LidarRange * 0.9;
+filter_radius = LidarRange * 1;
 theta_offset = deg2rad(4);
 
 rejection_setting = 0.3;
 converge_metric = 1e-4;
 
 frame = 1;
-frame_skip = 50;
+frame_skip = 15;
 
 %reset the yaw if we need to
 if IMU_Yaw(1) ~= 0
     IMU_Yaw = IMU_Yaw - IMU_Yaw(1);
 end
-for nScan = 1:frame_skip:size(nScanIndex)
+for nScan = 50:frame_skip:10000000
     
     dp = [[0 1];[0 0]];
-    
+
     % Retrieve each scan's points
     nIndex = nScanIndex(nScan);
     I = nIndex == Lidar_ScanIndex;
@@ -66,11 +68,12 @@ for nScan = 1:frame_skip:size(nScanIndex)
     
     stamp = Lidar_Timestamp(I);
     [~,stamp_idx] = min( abs(stamp(1)-IMU_Timestamp));
+    timestamps(frame) = stamp(1);
     
     yaw = IMU_Yaw(stamp_idx);
     
     %  Remove out of range measurements
-    I = (z >= LidarRange*0.9);
+    I = (z >= LidarRange*1);
     a(I) = [];
     z(I) = [];
     
@@ -102,8 +105,8 @@ for nScan = 1:frame_skip:size(nScanIndex)
         world = [world raw];
         
         %update the graph and get our next map
-        [ graph, ndx, ndy, map ] = update_graph( graph, raw, ndx, ndy, pose);
-        %map = raw;
+        %[ graph, ndx, ndy, map ] = update_graph( graph, raw, ndx, ndy, pose);
+        map = raw;
         
     end
     
@@ -113,7 +116,7 @@ for nScan = 1:frame_skip:size(nScanIndex)
     plot_scans
     
     %not really using, but may be helpful in the future
-    frame = frame + 1;
+    frame = frame + 1
 end
 
 %plot_scans
